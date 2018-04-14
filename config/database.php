3a3f2523081d9b542c4299edddae02fcf8638523
LasterSlaster <lasterslaster@gmx.de>
2018-04-14 13:18:14 +0200

    Added database configuration for postgress sql db on heroku

--- config/database.php
+++ config/database.php
@@ -1,5 +1,11 @@
 <?php
 
+$url = parse_url(getenv("DATABASE_URL"));
+$host = $url["host"]??null;
+$user = $url["user"]??null;
+$password = $url["pass"]??null;
+$database = substr($url["path"], 1)??null;
+
 return [
 
     /*
@@ -13,7 +19,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql_production'), //INFO: pgsql_production only for production (master branch) on heroku elsewhere use mysql
 
     /*
     |--------------------------------------------------------------------------
@@ -66,6 +72,17 @@ return [
             'schema' => 'public',
             'sslmode' => 'prefer',
         ],
+
+        'pgsql_production' => [
+            'driver' => 'pgsql',
+            'host' => $host,
+            'database' => $database,
+            'username' => $user,
+            'password' => $password,
+            'charset' => 'utf8',
+            'prefix' => '',
+            'schema' => 'public',
+        ] 
 
         'sqlsrv' => [
             'driver' => 'sqlsrv',
