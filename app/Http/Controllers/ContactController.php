ec930b0e24b4659dc14f980fe89c6c91b78817b7
LasterSlaster <lasterslaster@gmx.de>
2018-06-19 19:12:12 +0200

    Fixed missing dependency

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\ContactUs;
-use Illuminate\Http\Response;
+use Illuminate\Http\Request;
 
 /**
  * Class ContactController - Controller for contact requests.
