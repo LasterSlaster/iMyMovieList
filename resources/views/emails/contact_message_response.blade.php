95abeb449ccf0bd45a47e475d91baebe155d989a
LasterSlaster <lasterslaster@gmx.de>
2018-06-19 20:02:09 +0200

    Added blades for email content

--- resources/views/emails/contact_message_response.blade.php
+++ resources/views/emails/contact_message_response.blade.php
@@ -1,3 +1,10 @@
-<p>Sie haben eine Kontaktanfrage von {{name}} mit der Email Adresse {{email}}</p>
+<h1>Deine Konstaktanfrage an IMyMovieList</h1>
+<p>Hallo {{$name}}</p>
+<p>Danke für deine Kontaktanfrage!</p>
+<p>Wir melden uns so schnell wie möglich bei dir.</p>
+<h2>Deine Kontaktanfrage:</h2>
+<p>Dein Name: {{$name}}</p>
+<p>Deine Emailadresse: {{$email}}</p>
+<p>Deine Nachricht an uns: </p>
 <br>
 <p>{{$user_message}}</p>
