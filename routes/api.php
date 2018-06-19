ef13a5d3af14735fd40970f327b15f82108bed60
LasterSlaster <lasterslaster@gmx.de>
2018-06-19 19:09:48 +0200

    Fixed typo

--- routes/api.php
+++ routes/api.php
@@ -20,7 +20,7 @@ use Illuminate\Http\Request;
 Route::post('/user/signup', 'UserController@signup');
 Route::post('/user/signin', 'UserController@signin');
 Route::post('/passwords/reset/{token?}', 'UserController@forgotpw');
-Route::post('/contact', 'ContractController@sendEmail');
+Route::post('/contact', 'ContactController@sendEmail');
 
 Route::middleware('auth.jwt')->group(function() {
 
