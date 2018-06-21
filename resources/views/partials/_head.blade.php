56eb7374ba782b2434cc4241c17a8af5d1b6727d
LasterSlaster <lasterslaster@gmx.de>
2018-06-21 16:55:07 +0200

    CSRF token to landing page

--- resources/views/partials/_head.blade.php
+++ resources/views/partials/_head.blade.php
@@ -1,13 +1,14 @@
 <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="utf-8"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1"/>
     <meta name="google-site-verification" content="ybtcn8zV0i1ybCnSxO4TLTvPQ-U4yLJ2EXIlREVIG_E" />
     <meta name="description" content="Finde aktuelle Kinotitel und verwalte deine lieblingsfilme bequem auf unserer Seite"/>
     <meta name="author" content="iMyMovieList"/>
     <meta name="copyright" content="iMyMovieList"/>
     <meta name="keywords" content="filme, entdecken, aktuelle filme, filme verwalten, organisieren, bewerten, filme organisieren, beste filme, trailer anschauen, film empfehlungen"/>
-    <title>iMyMovieList</title> 
+    <meta name="csrf-token" content="{{ csrf_token() }}"/>
+    <title>iMyMovieList</title>
     <link rel="icon" type="image/x-icon" href="/img/imml_logo_tab.png">
     <link href="/css/app.css" type="text/css" rel="stylesheet">
     <link href="/css/nav.css" type="text/css" rel="stylesheet">
