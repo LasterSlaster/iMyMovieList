148c1dffac120320b4294ae90ab068546d4f09c2
LasterSlaster <lasterslaster@gmx.de>
2018-06-21 15:46:32 +0200

    Added google verification meta tag

--- resources/views/partials/_head.blade.php
+++ resources/views/partials/_head.blade.php
@@ -2,6 +2,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="google-site-verification" content="ybtcn8zV0i1ybCnSxO4TLTvPQ-U4yLJ2EXIlREVIG_E" />
     <title>iMyMovieList</title> 
     <link rel="icon" type="image/x-icon" href="/img/imml_logo_tab.png">
     <link href="/css/app.css" type="text/css" rel="stylesheet">
