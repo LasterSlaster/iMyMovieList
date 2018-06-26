4cfe6e03a5eaf204be7d93081130dbfbadbc9e43
LasterSlaster <lasterslaster@gmx.de>
2018-06-26 19:49:05 +0200

    Changes to seeding!

--- database/seeds/UserMovieRatingsTableSeeder.php
+++ database/seeds/UserMovieRatingsTableSeeder.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\UserMovieRating;
+use Faker\Generator as Faker;
+use App\SeenListMovie;
 
 class UserMovieRatingsTableSeeder extends Seeder
 {
@@ -11,6 +14,14 @@ class UserMovieRatingsTableSeeder extends Seeder
      */
     public function run()
     {
-        factory(App\UserMovieRating::class, 50)->create();
+        $seenlistMovies = SeenListMovie::all();
+
+        $seenlistMovies->each(function($u) {
+            $rating = new UserMovieRating();
+            $rating->user_id = $u->seen_list_id()->user_id;
+            $rating->movie_id = $u->movie_id;
+            $rating->rating = 2;
+            $rating->save();
+        });
     }
 }
--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -13,7 +13,7 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        factory(App\User::class, 50)->create()
+        factory(App\User::class, 5)->create()
             ->each(function($u) {
                 $u->watchList()->save(new WatchList(['user_id' => $u->id]));
                 $u->seenList()->save(new SeenList(['user_id' => $u->id]));
